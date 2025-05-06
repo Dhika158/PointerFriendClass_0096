91f521bc6d45639bfbb671288080bd84137ac17b
Dhika158 <andhikanurjamil7@gmail.com>
2025-05-06 15:08:33 +0700

    Membuat Onjek

--- pointerfriend.cpp
+++ pointerfriend.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class PresegiPanjang 
+class PersegiPanjang 
 {
    public: // akses modifier
      int panjang, lebar;
@@ -41,3 +41,16 @@ class Lingkaran
         return 3.14 * r * r;
     }
 };
+
+int main()
+{
+    PersegiPanjang objekPP; //membuat objek
+    Lingkaran ol;
+
+    cout << "Masukkan panjang : ";
+    cin >> objekPP.panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> objekPP.lebar;
+    cout << "Luas Pesegi Panjang : "; 
+    objekPP.luasPp(objekPP.panjang, objekPP.lebar) << endl;
+}
